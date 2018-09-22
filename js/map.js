25faa597a3b9d62f04d20a6f449f6acee3c3a8fe
Копытин Олег Николаевич <o.kopytin@hq.qiwi.com>
2018-09-22 21:54:01 +0300

    удалил перемещение метки

--- js/map.js
+++ js/map.js
@@ -339,47 +339,6 @@ timeoutElemet.addEventListener('input', function (evt) {
   timeinElement.value = target;
 });
 
-var updateCoordinte = function (evt) {
-  evt.preventDefault();
-  var startCoords = {
-    x: evt.clientX,
-    y: evt.clientY
-  };
-  var dragged = false;
-  var onMouseMove = function (moveEvt) {
-    moveEvt.preventDefault();
-    dragged = true;
-    var shift = {
-      x: startCoords.x - moveEvt.clientX,
-      y: startCoords.y - moveEvt.clientY
-    };
-    startCoords = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
-    };
-    dialogHandler.style.top = (dialogHandler.offsetTop - shift.y) + 'px';
-    dialogHandler.style.left = (dialogHandler.offsetLeft - shift.x) + 'px';
-  };
-
-  var onMouseUp = function (upEvt) {
-    upEvt.preventDefault();
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
-    if (dragged) {
-      var onClickPreventDefault = function (clickevt) {
-        clickevt.preventDefault();
-        dialogHandler.removeEventListener('click', onClickPreventDefault);
-      };
-      dialogHandler.addEventListener('click', onClickPreventDefault);
-    }
-
-  };
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
-};
-
-dialogHandler.addEventListener('mousedown', updateCoordinte);
-
 roomNumber.addEventListener('input', function (evt) {
   fieldsetDisabledEnabled(capacity, false);
   var roomNum = evt.currentTarget.value;
